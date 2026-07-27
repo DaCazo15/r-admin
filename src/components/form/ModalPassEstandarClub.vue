e65d90b09e1d4b061ea3a6ef33c6767d4f630e3b
DaCazo15 <dcazorla.0190@gmail.com>
2026-07-27 02:08:39 -0400

    change

--- src/components/form/ModalPassEstandarClub.vue
+++ src/components/form/ModalPassEstandarClub.vue
@@ -42,6 +42,16 @@ const cambioEstado = computed(() => {
 
       <!-- Contenido del modal -->
       <form @submit.prevent="guardar" class="p-6 space-y-4">
+        <div class="border-l-2 border-gray-500 bg-gray-100 p-4 rounded-br-lg rounded-tr-lg">
+          <h5 class="text-[16px] font-medium text-primary-600">Nota:</h5>
+          <p class="text-[16px] font-light text-gray-500">
+            Esta contraseña será asignada a los miembros del club, con ella podran iniciar sesión
+            por primera vez.
+            <span class="font-normal text-red-700"
+              >Se recomienda que luego de iniciar sesión, el socio coloque una personal.</span
+            >
+          </p>
+        </div>
         <div class="flex gap-4 items-center justify-center">
           <p class="text-primary-600 font-medium">Password Actual</p>
           <input
