a399a88fd26a60a0596b117485b5e9ccd096fc27
DaCazo15 <dcazorla.0190@gmail.com>
2026-07-28 12:39:17 -0400

    change

--- src/components/footer/FooterComponent.vue
+++ src/components/footer/FooterComponent.vue
@@ -3,26 +3,26 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <footer class="text-xs text-gray-400 font-medium text-center mt-6">
+  <footer class="text-[10px] sm:text-xs text-gray-400 font-medium text-center mt-6">
     Rotaract 4370 &middot; R-Admin &middot; Todos los derechos reservados
     <div class="flex sm:gap-3 gap-1 w-full justify-center mt-3 mb-10">
       <router-link
         :to="{ name: 'conditions' }"
-        class="text-gray-400 cursor-pointer text-[10px] sm:text-xs py-1 px-2 hover:text-primary-600 font-medium"
+        class="text-gray-400 cursor-pointer text-[10px] sm:text-xs py-1 px-2 hover:text-primary-600 active:text-primary-600 font-medium"
       >
         Condiciones del servicio
       </router-link>
       <span class="text-gray-400 font-semibold">|</span>
       <router-link
         :to="{ name: 'policies' }"
-        class="text-gray-400 cursor-pointer text-[10px] sm:text-xs py-1 px-2 hover:text-primary-600 font-medium"
+        class="text-gray-400 cursor-pointer text-[10px] sm:text-xs py-1 px-2 hover:text-primary-600 active:text-primary-600 font-medium"
       >
         Política de privacidad
       </router-link>
       <span class="text-gray-400 font-semibold">|</span>
       <router-link
         :to="{ name: 'info' }"
-        class="text-gray-400 cursor-pointer text-[10px] sm:text-xs py-1 px-2 hover:text-primary-600 font-medium"
+        class="text-gray-400 cursor-pointer text-[10px] sm:text-xs py-1 px-2 hover:text-primary-600 active:text-primary-600 font-medium"
       >
         Información
       </router-link>
