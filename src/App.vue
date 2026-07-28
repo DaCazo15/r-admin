de2cca79a832dae419e6bd2ea3189ef24ebf81f3
DaCazo15 <dcazorla.0190@gmail.com>
2026-07-28 12:37:43 -0400

    change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { RouterView, useRoute } from 'vue-router'
 import { storeToRefs } from 'pinia'
-import NavBar from './components/NavComponent.vue'
 import { useSesionStore } from './stores/useSesionStore'
+import NavBar from './components/NavComponent.vue'
+import Footer from './components/footer/FooterComponent.vue'
 
 const route = useRoute()
 const sesionStore = useSesionStore()
@@ -18,30 +19,6 @@ const { usuario, cargando } = storeToRefs(sesionStore)
     <div class="pb-4 pt-20 py-4">
       <RouterView />
     </div>
-    <footer class="text-xs text-gray-400 font-medium text-center mt-6">
-      Rotaract 4370 &middot; R-Admin &middot; Todos los derechos reservados
-      <div class="flex gap-3 w-full justify-center mt-3 mb-10">
-        <router-link
-          :to="{ name: 'conditions' }"
-          class="text-gray-400 cursor-pointer text-xs py-1 px-2 hover:text-primary-600 font-medium"
-        >
-          Condiciones del servicio
-        </router-link>
-        <span class="text-gray-400 font-semibold">|</span>
-        <router-link
-          :to="{ name: 'policies' }"
-          class="text-gray-400 cursor-pointer text-xs py-1 px-2 hover:text-primary-600 font-medium"
-        >
-          Política de privacidad
-        </router-link>
-        <span class="text-gray-400 font-semibold">|</span>
-        <router-link
-          :to="{ name: 'info' }"
-          class="text-gray-400 cursor-pointer text-xs py-1 px-2 hover:text-primary-600 font-medium"
-        >
-          Información
-        </router-link>
-      </div>
-    </footer>
+    <Footer />
   </template>
 </template>
