eb772a6f2f21845fa9fee307f425235de04b5c32
DaCazo15 <dcazorla.0190@gmail.com>
2026-07-29 02:51:58 -0400

    r-admin-finished-continue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 import { RouterView, useRoute } from 'vue-router'
 import { storeToRefs } from 'pinia'
 import { useSesionStore } from './stores/useSesionStore'
